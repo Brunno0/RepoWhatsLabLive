b51d9e010cfe472a5b31d6661a1d436decdbc4c1
br <brunno00amorim@gmail.com>
2023-09-04 14:18:31 -0300

    [FEAT] base para live

---
+++ src/components/Chat.jsx
@@ -0,0 +1,14 @@
+
+import MessageForm from './MessageForm';
+import { AppContainer} from '../AppStyled';
+
+function Chat() {
+    
+    return (
+        <AppContainer>
+            <MessageForm />
+        </AppContainer>
+    );
+}
+
+export default Chat;
---
+++ src/components/Message.jsx
@@ -0,0 +1,12 @@
+import { MessageBox, MessageContainer } from "./StyledMessage";
+
+function Message() {
+
+    <MessageContainer>
+        <MessageBox >
+            Messagem
+        </MessageBox>
+    </MessageContainer>
+}
+
+export default Message;
